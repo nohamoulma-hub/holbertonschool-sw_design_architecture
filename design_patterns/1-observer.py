a16d62c19607f46f5a280ecd5877700482abd364
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-20 11:21:07 +0200

    refactor: format subscribe method for improved readability

--- design_patterns/1-observer.py
+++ design_patterns/1-observer.py
@@ -11,7 +11,11 @@ class NewsSubject:
     def __init__(self) -> None:
         self._subs: dict[Observer, set[str] | None] = {}
 
-    def subscribe(self, observer: Observer, topics: set[str] | None = None) -> None:
+    def subscribe(
+            self,
+            observer: Observer,
+            topics: set[str] | None = None
+            ) -> None:
         if observer in self._subs:
             return  # ignore duplicate subscribe for same instance
         self._subs[observer] = topics
